6195c118829a2eaeb33a6fb42bce2bb4a862ac79
Julian Vivas <87610014+JulCode@users.noreply.github.com>
2022-05-23 12:08:11 -0500

    .

---
+++ micro-code/.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/julio/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/julio/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/julio/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/config/sdkconfig.h
