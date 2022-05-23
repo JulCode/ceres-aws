59eee16b3e6c95606b82e38f6a4c19c50e0f27d2
Julian Vivas <87610014+JulCode@users.noreply.github.com>
2022-05-23 11:35:39 -0500

    add-microCode

---
+++ micro-code/src/main.cpp
@@ -0,0 +1,119 @@
+//librerias
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+//sensor de humedad y temperatura
+#include <DHT_U.h>
+#include "DHT.h"
+#define DHTPIN 23    
+#define DHTTYPE DHT11   // DHT 11
+ 
+#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+ 
+float h ;
+float t;
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+WiFiClientSecure net = WiFiClientSecure();
+PubSubClient client(net);
+ 
+ //protocolo conexión AWS
+void connectAWS()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ 
+  Serial.println("Connecting to Wi-Fi");
+ 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  // Configuracion credenciales AWS
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+ 
+  // Conexion al broker MQTT de AWS
+  client.setServer(AWS_IOT_ENDPOINT, 8883);
+ 
+  
+ 
+  Serial.println("Connecting to AWS IOT");
+ 
+  while (!client.connect(THINGNAME))
+  {
+    Serial.print(".");
+    delay(100);
+  }
+ 
+  if (!client.connected())
+  {
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+ 
+  // Suscribirse a un topico
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+ 
+  Serial.println("AWS IoT Connected!");
+}
+ 
+void publishMessage()
+{
+  StaticJsonDocument<200> doc;
+  doc["humidity"] = h;
+  doc["temperature"] = t;
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); //Imprimir al cliente
+ 
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+ 
+void messageHandler(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("incoming: ");
+  Serial.println(topic);
+ 
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload);
+  const char* message = doc["message"];
+  Serial.println(message);
+}
+ 
+void setup()
+{
+  Serial.begin(9600);
+  connectAWS();
+  dht.begin();
+}
+ 
+void loop()
+{
+  //inicialización variable humedad y temperatura
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+ 
+ // Verificaa si las lecturas están correctas
+  if (isnan(h) || isnan(t) )  
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+ //imprimir temperatura y humedad
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+ 
+  publishMessage();
+  client.loop();
+  delay(1000);
+}
